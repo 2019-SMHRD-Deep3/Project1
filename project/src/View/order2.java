6bfbcd7b3611d84edb5e11378f21428a94017737
SM026 <SM026@DESKTOP-RCJTJDL>
2020-01-17 22:00:41 +0900

    사장님 창

--- project/src/View/order2.java
+++ project/src/View/order2.java
@@ -34,7 +34,8 @@ public class order2 {
 		frame.setBounds(400, 150, 1011, 661);
 		frame.setVisible(true);
 		
-		
+		String c1 = this.getClass().getResource(".").getPath() + "..//..//img//c.png";
+
 		JPanel panel_1 =new JPanel();
 		panel_1.setBounds(371, 0, 634, 632);
 		frame.getContentPane().add(panel_1);
@@ -70,6 +71,7 @@ public class order2 {
 		
 		
 		
+		
 		JPanel panel_4 = new JPanel();
 		panel_4.setBackground(Color.WHITE);
 		panel_4.setBounds(279, 0, 716, 620);
@@ -84,30 +86,20 @@ public class order2 {
 		panel_4.add(main, "name_1749428196331400");
 		main.setLayout(null);
 		
-		JButton button = new JButton("\uAC00\uAC8C \uC815\uBCF4 \uC218\uC815");
-		
-		button.setBounds(52, 146, 125, 111);
-		main.add(button);
-		
-		JButton button_1 = new JButton("\uBA54\uB274\uC218\uC815");
-		
-		button_1.setBounds(265, 146, 125, 111);
-		main.add(button_1);
-		
 		JButton button_3 = new JButton("\uCD1D \uB9E4\uCD9C");
-		button_3.setBounds(52, 368, 125, 111);
+		button_3.setBounds(67, 499, 125, 111);
 		main.add(button_3);
 		
 		JButton button_4 = new JButton("\uB9AC\uBDF0/\uB9AC\uD50C \uAD00\uB9AC");
-		button_4.setBounds(265, 368, 125, 111);
+		button_4.setBounds(280, 499, 125, 111);
 		main.add(button_4);
 		
 		JButton button_5 = new JButton("\uC8FC\uBB38 \uC811\uC218\uCC3D");
-		button_5.setBounds(465, 368, 125, 111);
+		button_5.setBounds(480, 499, 125, 111);
 		main.add(button_5);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(465, 146, 125, 111);
+		panel.setBounds(0, 475, 125, 111);
 		main.add(panel);
 		panel.setLayout(null);
 		
@@ -148,14 +140,179 @@ public class order2 {
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		JPanel panel_6 = new JPanel();
+		panel_6.setBackground(new Color(224, 255, 255));
 		panel_6.setBounds(52, 22, 589, 79);
 		main.add(panel_6);
 		panel_6.setLayout(null);
 		
-		JLabel lblNewLabel_5 = new JLabel("\uC0AC\uC7A5\uB2D8 ");
+		JLabel lblNewLabel_5 = new JLabel("\uD55C\uC1A5 \uAD11\uC8FC\uB300\uC810");
+		lblNewLabel_5.setBackground(new Color(255, 255, 255));
 		lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_5.setBounds(0, 0, 589, 79);
 		panel_6.add(lblNewLabel_5);
+		
+		JPanel panel_34 = new JPanel();
+		panel_34.setBackground(Color.WHITE);
+		panel_34.setBounds(88, 146, 137, 135);
+		main.add(panel_34);
+		panel_34.setLayout(null);
+		
+		JLabel lblNewLabel_4 = new JLabel("\uAC00\uAC8C \uC815\uBCF4 \uC218\uC815");
+		lblNewLabel_4.setFont(new Font("HY수평선B", Font.BOLD, 15));
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_4.setBounds(0, 25, 137, 82);
+		panel_34.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setBounds(0, 0, 137, 135);
+		panel_34.add(lblNewLabel_3);
+		lblNewLabel_3.setIcon(new ImageIcon(c1));
+		
+		JPanel panel_35 = new JPanel();
+		panel_35.setLayout(null);
+		panel_35.setBackground(Color.WHITE);
+		panel_35.setBounds(275, 146, 137, 135);
+		main.add(panel_35);
+		
+		JLabel label_48 = new JLabel("\uBA54\uB274 \uC218\uC815");
+		label_48.setHorizontalAlignment(SwingConstants.CENTER);
+		label_48.setFont(new Font("HY수평선B", Font.BOLD, 15));
+		label_48.setBounds(0, 25, 137, 82);
+		panel_35.add(label_48);
+		
+		JLabel label_49 = new JLabel("");
+		label_49.setBounds(0, 0, 137, 135);
+		panel_35.add(label_49);
+		label_49.setIcon(new ImageIcon(c1));
+		
+		JPanel pay = new JPanel();
+		pay.setLayout(null);
+		pay.setBackground(Color.WHITE);
+		panel_4.add(pay, "name_1749820970039500");
+		
+		JPanel panel_37 = new JPanel();
+		panel_37.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				panel_37.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+								panel_4.removeAll();
+								panel_4.add(pay);
+								panel_4.repaint();
+								panel_4.revalidate();
+						
+					}
+				});
+			}
+		});
+		panel_37.setLayout(null);
+		panel_37.setBackground(Color.WHITE);
+		panel_37.setBounds(88, 302, 137, 135);
+		main.add(panel_37);
+		
+		JLabel label_52 = new JLabel("\uCD1D \uB9E4\uCD9C");
+		label_52.setHorizontalAlignment(SwingConstants.CENTER);
+		label_52.setFont(new Font("HY수평선B", Font.BOLD, 15));
+		label_52.setBounds(0, 25, 137, 82);
+		panel_37.add(label_52);
+		
+		JLabel label_53 = new JLabel("");
+		label_53.setBounds(0, 0, 137, 135);
+		panel_37.add(label_53);
+		label_53.setIcon(new ImageIcon(c1));
+		
+		JPanel panel_38 = new JPanel();
+		panel_38.setLayout(null);
+		panel_38.setBackground(Color.WHITE);
+		panel_38.setBounds(275, 302, 137, 135);
+		main.add(panel_38);
+		
+		JLabel label_54 = new JLabel("\uB9AC\uBDF0/\uB9AC\uD50C \uAD00\uB9AC");
+		label_54.setHorizontalAlignment(SwingConstants.CENTER);
+		label_54.setFont(new Font("HY수평선B", Font.BOLD, 15));
+		label_54.setBounds(0, 25, 137, 82);
+		panel_38.add(label_54);
+		
+		JLabel label_55 = new JLabel("");
+		label_55.setBounds(0, 0, 137, 135);
+		panel_38.add(label_55);
+		label_55.setIcon(new ImageIcon(c1));
+		
+		JPanel panel_39 = new JPanel();
+		panel_39.setLayout(null);
+		panel_39.setBackground(Color.WHITE);
+		panel_39.setBounds(464, 302, 137, 135);
+		main.add(panel_39);
+		
+		JLabel label_56 = new JLabel("\uC8FC\uBB38 \uC811\uC218\uCC3D");
+		label_56.setHorizontalAlignment(SwingConstants.CENTER);
+		label_56.setFont(new Font("HY수평선B", Font.BOLD, 15));
+		label_56.setBounds(0, 25, 137, 82);
+		panel_39.add(label_56);
+		
+		JLabel label_57 = new JLabel("");
+		label_57.setBounds(0, 0, 137, 135);
+		panel_39.add(label_57);
+		label_57.setIcon(new ImageIcon(c1));
+		
+		JPanel panel_40 = new JPanel();
+		panel_40.setBounds(461, 146, 137, 135);
+		main.add(panel_40);
+		panel_40.setLayout(new CardLayout(0, 0));
+		
+		JPanel panel_36 = new JPanel();
+		JPanel panel_41 = new JPanel();
+		panel_36.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				panel_40.removeAll();
+				panel_40.add(panel_41);
+				panel_40.repaint();
+				panel_40.revalidate();
+			}
+		});
+		panel_40.add(panel_36, "name_1229935410162600");
+		panel_36.setLayout(null);
+		panel_36.setBackground(Color.WHITE);
+		
+		panel_41.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				panel_40.removeAll();
+				panel_40.add(panel_36);
+				panel_40.repaint();
+				panel_40.revalidate();
+				
+			}
+		});
+		panel_40.add(panel_41, "name_1229955962227900");
+		panel_41.setLayout(null);
+		panel_41.setBackground(Color.WHITE);
+		
+		JLabel label_58 = new JLabel("\uC601\uC5C5\uC2DC\uC791");
+		label_58.setHorizontalAlignment(SwingConstants.CENTER);
+		label_58.setFont(new Font("HY수평선B", Font.BOLD, 15));
+		label_58.setBounds(0, 25, 137, 82);
+		panel_41.add(label_58);
+		
+		JLabel label_59 = new JLabel("");
+		label_59.setBounds(0, 0, 137, 135);
+		panel_41.add(label_59);
+		label_59.setIcon(new ImageIcon(c1));
+		
+		
+		
+		JLabel label_50 = new JLabel("\uC601\uC5C5\uC885\uB8CC");
+		label_50.setHorizontalAlignment(SwingConstants.CENTER);
+		label_50.setFont(new Font("HY수평선B", Font.BOLD, 15));
+		label_50.setBounds(0, 25, 137, 82);
+		panel_36.add(label_50);
+		
+		JLabel label_51 = new JLabel("");
+		label_51.setBounds(0, 0, 137, 135);
+		panel_36.add(label_51);
+		label_51.setIcon(new ImageIcon(c1));
 		
 		JPanel store1 = new JPanel();
 		store1.setLayout(null);
@@ -212,9 +369,9 @@ public class order2 {
 				
 			}
 		});
-		String shopImg1 = this.getClass().getResource(".").getPath() + "..//..//img//s1.png";
+		String shopimg1 = this.getClass().getResource(".").getPath() + "..//..//img//s1.png";
+		store2.setIcon(new ImageIcon(shopimg1));
 		panel_7.setLayout(null);
-		store2.setIcon(new ImageIcon(shopImg1));
 		panel_7.add(store2);
 		
 
@@ -660,11 +817,7 @@ public class order2 {
 		});
 		button_8.setBounds(69, 30, 125, 111);
 		work_start.add(button_8);
-			
-		JPanel pay = new JPanel();
-		pay.setLayout(null);
-		pay.setBackground(Color.WHITE);
-		panel_4.add(pay, "name_1749820970039500");
+		
 		
 		JButton button_9 = new JButton("\uC774\uC804\uC73C\uB85C");
 		button_9.addActionListener(new ActionListener() {
@@ -726,38 +879,7 @@ public class order2 {
 		});
 		button_11.setBounds(69, 30, 125, 111);
 		order.add(button_11);
-		
-		button.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				panel_4.removeAll();
-				panel_4.add(store1);
-				panel_4.repaint();
-				panel_4.revalidate();
-			}
-		});
-		
-		
-		button_1.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				panel_4.removeAll();
-				panel_4.add(menu);
-				panel_4.repaint();
-				panel_4.revalidate();
-			}
-		});
-		
-		button_3.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-						panel_4.removeAll();
-						panel_4.add(pay);
-						panel_4.repaint();
-						panel_4.revalidate();
-				
-			}
-		});
+	
 		
 		button_4.addMouseListener(new MouseAdapter() {
 			@Override
